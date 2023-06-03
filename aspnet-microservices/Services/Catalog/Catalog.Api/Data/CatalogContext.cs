5c8daea697083fc0849a82d0a5adf4703946da5d
Mohammad Mohamadi <mohammad.mohamadi@dotnek.com>
2023-06-04 02:07:56 +0330

    Config Database Mongo

---
+++ aspnet-microservices/Services/Catalog/Catalog.Api/Data/CatalogContext.cs
@@ -0,0 +1,20 @@
+﻿using Catalog.Api.Entities;
+using Microsoft.Extensions.Configuration;
+using MongoDB.Driver;
+
+namespace Catalog.Api.Data
+{
+    public class CatalogContext : ICatalogContext
+    {
+        public IMongoCollection<Product> Products { get; }
+
+        public CatalogContext(IConfiguration _configuration)
+        {
+            var client = new MongoClient(_configuration.GetValue<string>("DatabaseSetting:ConnectionString"));
+            var database = client.GetDatabase(_configuration.GetValue<string>("DatabaseSetting:DatabaseName"));
+            Products = database.GetCollection<Product>(_configuration.GetValue<string>("DatabaseSetting:CollectionName"));
+            CatalogContextSeed.SeedData(Products);
+        }
+    }
+}
+ 
--- aspnet-microservices/Services/Catalog/Catalog.Api/Startup.cs
+++ aspnet-microservices/Services/Catalog/Catalog.Api/Startup.cs
@@ -23,7 +23,7 @@ namespace Catalog.Api
         public IConfiguration Configuration { get; }
 
         // This method gets called by the runtime. Use this method to add services to the container.
-        public void ConfigureServices(IServiceCollection services)
+        public void ConfigureServices(IServiceCollection services) 
         {
 
             services.AddControllers();
