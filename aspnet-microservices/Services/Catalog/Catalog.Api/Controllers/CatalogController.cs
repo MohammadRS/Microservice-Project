a53f376e62512f681940a2cf30cfcf9f2ebf52eb
Mohammad Mohamadi <mohammad.mohamadi@dotnek.com>
2023-06-13 01:20:53 +0330

    Containerze Project and Config Docker Component

--- aspnet-microservices/Services/Catalog/Catalog.Api/Controllers/CatalogController.cs
+++ aspnet-microservices/Services/Catalog/Catalog.Api/Controllers/CatalogController.cs
@@ -32,20 +32,21 @@ namespace Catalog.Api.Controllers
         [HttpGet("{id:length(24)}", Name = "GetProduct")]
         [ProducesResponseType((int)HttpStatusCode.NotFound)]
         [ProducesResponseType(typeof(Product), (int)HttpStatusCode.OK)]
-        public async Task<ActionResult<IEnumerable<Product>>> GetProductById(string id)
+        public async Task<ActionResult<IEnumerable<Product>>> GetProducts(string id)
         {
             var product = await _productRepository.GetProduct(id);
             if (product == null)
             {
-                _logger.LogError($"product with id = {id} is not found");
+                _logger.LogError($"product with id: {id} is not found");
                 return NotFound();
             }
+
             return Ok(product);
         }
 
-        [HttpGet("[action]/{category]")]
-        [ProducesResponseType(typeof(Product), (int)HttpStatusCode.OK)]
-        public async Task<ActionResult<IEnumerable<Product>>> GetProductBycategory(string category)
+        [HttpGet("[action]/{category}")]
+        [ProducesResponseType(typeof(IEnumerable<Product>), (int)HttpStatusCode.OK)]
+        public async Task<ActionResult<IEnumerable<Product>>> GetProductsByCategory(string category)
         {
             var products = await _productRepository.GetProductByCategory(category);
             return Ok(products);
@@ -58,6 +59,7 @@ namespace Catalog.Api.Controllers
             await _productRepository.CreateProduct(product);
             return CreatedAtRoute("GetProduct", new { id = product.Id }, product);
         }
+
         [HttpPut]
         [ProducesResponseType(typeof(Product), (int)HttpStatusCode.OK)]
         public async Task<IActionResult> UpdateProduct([FromBody] Product product)
